d2546f59a51385748345030f26688bf3ef022309
guxiaowu <337118043@qq.com>
2018-10-01 15:34:04 +0800

    update 72

---
+++ 51-100/72.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+    const len1 = word1.length;
+    const len2 = word2.length;
+    const dp = new Array(len1+1);
+
+    for (let i = 0; i <= len1; i++){        //base
+        dp[i] = new Array(len2+2);
+        dp[i][0] = i;
+    }
+        
+    for (let j  =0; j <= len2;j++)
+        dp[0][j] = j;
+    for(let i = 1;i <= len1 ; i++)          
+        for(let j = 1;j<= len2;j++){
+            if(word1[i-1] == word2[j-1])
+                dp[i][j] = dp[i-1][j-1];
+            else
+                dp[i][j] = dp[i-1][j-1] +1;
+            //update function
+            dp[i][j] = Math.min(dp[i][j],dp[i-1][j]+1,dp[i][j-1]+1);
+        }
+    return dp[len1][len2];
+};
